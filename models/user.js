bcc7e4a4842f2b012425eb6fc6231a7f2167e0c0
hectorcoronado <coronado.hector@gmail.com>
2017-04-27 16:16:23 -0700

    encrypts password with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
-// define model:
 const userSchema = new Schema({
   email: {
     type: String,
@@ -11,8 +11,23 @@ const userSchema = new Schema({
   password: String
 });
 
-// create model class:
+// on save hook, encrypt password:
+userSchema.pre('save', function(next) {
+  const user = this; // access user model
+
+  // generate salt:
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) { return next(err); }
+    // hash/encrypt pw with salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if(err) {return next(err); }
+      // overwrite plain text pw w encryption
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 const userModel = mongoose.model('user', userSchema);
 
-// export model:
 module.exports = userModel;
